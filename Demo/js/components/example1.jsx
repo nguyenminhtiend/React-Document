8d7e08c7834e5ba9ce3bc0cfd380e90ce91fe070
nguyenminhtiend <nguyenminhtiend@gmail.com>
2015-12-04 18:41:35 +0700

    update

---
+++ Demo/js/components/example1.jsx
@@ -0,0 +1,8 @@
+﻿
+var Example1 = React.createClass({
+    render: function () {
+        return (<h1 className="test">Hello Wrold11!</h1>);
+    }
+});
+
+ReactDOM.render(<Example1 />, document.getElementById("example"));
