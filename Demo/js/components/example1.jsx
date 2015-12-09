e93c614a3b0a370d2c45539f9f1724cebd11cbf2
nguyenminhtiend <nguyenminhtiend@gmail.com>
2015-12-09 17:44:00 +0700

    #

--- Demo/js/components/example1.jsx
+++ Demo/js/components/example1.jsx
@@ -1,26 +1,10 @@
 ﻿var MyComponent = React.createClass({
     render: function () {
         return (<div>
-           
-                <a href="#" class="normal">abc</a>&nbsp;&nbsp;&nbsp;
-                <a href="#">abc</a>&nbsp;&nbsp;&nbsp;
-                <a href="#">abc</a>&nbsp;&nbsp;&nbsp;
-                <a href="#">abc</a>&nbsp;&nbsp;&nbsp;
-                <a href="#">abc</a>&nbsp;&nbsp;&nbsp;
-                <a href="#">abc</a>&nbsp;&nbsp;&nbsp;
-
-                <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-    <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-    <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-    <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-
-    <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-    <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-    <a href="#" class="showmore">abc</a>&nbsp;&nbsp;&nbsp;
-                <a href="#" clas>abc</a>&nbsp;&nbsp;&nbsp;
-                
-            </div>);
+                Hello chu Hung!
+            </div>
+            );
     }
 });
 
-ReactDOM.render(<MyComponent />, document.getElementById("example"));
+React.render(<MyComponent />, document.getElementById("example"));
---
+++ Demo/js/components/reactElement.js
@@ -0,0 +1,5 @@
+﻿
+var child = React.createElement('li', null, 'Text Content');
+var root = React.createElement('ul', { className: 'my-list' }, child);
+
+React.render(root, document.getElementById('example'));
