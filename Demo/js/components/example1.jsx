4422bfc891d7d3a52bcf4502c3e39f94823ce95f
nguyenminhtiend <nguyenminhtiend@gmail.com>
2015-12-14 18:29:42 +0700

    #

--- Demo/js/components/example1.jsx
+++ Demo/js/components/example1.jsx
@@ -1,36 +1,43 @@
 ﻿
-var MyComponent = React.createClass({
-    getDefaultProps: function(){
-        return {
-            name: "messi"
-        };
-    },
-    render: function () {
-        return (<h1>Hello {this.props.name}!</h1>);
-    }
-});
 
-    var Counter = React.createClass({
-        incrementCount: function(){
-            this.setState({
-                count: this.state.count + 1
-            });
-        },
-        getInitialState: function(){
+    var DefaultNameMixin = {
+        getDefaultProps: function () {
             return {
-                count: 0
-            }
-        },
-        render: function(){
-            return (
-              <div className="my-component">
-                <h1>Count: {this.state.count}</h1>
-                <button type="button" onClick={this.incrementCount}>Increment</button>
-              </div>
-          );
+                name: "Messi"
+            };
+        }
+    };
+
+    var MyComponent = React.createClass({
+        mixins: [DefaultNameMixin],
+        render: function () {
+            return (<h1>Hello {this.props.name}!</h1>);
         }
     });
 
 
 
-React.render(<Counter />, document.getElementById("example"));
+    //var Counter = React.createClass({
+    //    incrementCount: function(){
+    //        this.setState({
+    //            count: this.state.count + 1
+    //        });
+    //    },
+    //    getInitialState: function(){
+    //        return {
+    //            count: 0
+    //        }
+    //    },
+    //    render: function(){
+    //        return (
+    //          <div className="my-component">
+    //            <h1>Count: {this.state.count}</h1>
+    //            <button type="button" onClick={this.incrementCount}>Increment</button>
+    //          </div>
+    //      );
+    //    }
+    //});
+
+
+
+React.render(<MyComponent />, document.getElementById("example"));
