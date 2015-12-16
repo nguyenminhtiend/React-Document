7aac55a9fc8650b7a6a95999c00af83efdb0f7ba
nguyenminhtiend <nguyenminhtiend@gmail.com>
2015-12-16 17:43:52 +0700

    #

--- Demo/js/components/lifeCycle.jsx
+++ Demo/js/components/lifeCycle.jsx
@@ -11,26 +11,62 @@
             number: 0
         };
     },
+    componentWillReceiveProps: function (nextProps) {
+        this.setState({ number: this.state.number + 2 });
+        console.log('componentWillReceiveProps');
+    },
     componentWillMount: function(){
         console.log('componentWillMount');
     },
     shouldComponentUpdate: function(nextProps, nextState){
         // return a boolean value
+        console.log('shouldComponentUpdate');
         return true;
+    },
+    componentWillUpdate: function(nextProps, nextState){
+        console.log('componentWillUpdate');
     },
     render: function () {
         console.log('render');
         return (<div>
-                    <h1>{this.state.number}</h1>
+                    <h1>{this.props.name}: {this.state.number}</h1>
                     <button onClick={this.countMe}>Count me</button>
                 </div>);
     },
     componentDidMount: function () {
         console.log('componentDidMount ');
     },
+    componentDidUpdate: function(prevProps, prevState){
+        console.log('componentDidUpdate');
+    },
     countMe: function () {
         this.setState({number: this.state.number + 1});
     }
 });
 
-React.render(<Component />, document.getElementById("example"));
+var App = React.createClass({
+    getInitialState: function () {
+        return {
+            name: "Test"
+        };
+    },
+    render: function () {
+        return (<div>
+            <button onClick={this.changeProps}>Change props</button>
+            <button onClick={this.unMount}>Unmount</button>
+            <Component name={this.state.name} />
+        </div>);
+    },
+    componentWillUnmount: function(){
+        console.log('componentWillUnmount');
+    },
+    changeProps: function () {
+        this.setState({name: "Test 1"});
+    },
+    unMount: function () {
+        React.unmountComponentAtNode(document.getElementById("example"));
+    }
+});
+
+
+React.render(<App />, document.getElementById("example"));
